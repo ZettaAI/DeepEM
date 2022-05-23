da30c72f1e9a2d304999ead98fe8c2574f84f0b9
Kisuk Lee <kisuk@princeton.edu>
2022-05-23 23:23:23 +0900

    update augmentation

--- deepem/data/augment/tilt_series/no_aug_val.py
+++ deepem/data/augment/tilt_series/no_aug_val.py
@@ -11,12 +11,16 @@ def get_augmentation(is_train, tilt_series=(0,0,0), tilt_series_crop=None,
 
     # Tilt series projection & label subsampling
     if tilt_series[0] > 0:
+        ts_n = tilt_series[0]
         ts_in = tilt_series[1]
         ts_out = tilt_series[2]
         assert ts_in > 0 and ts_out > 0
-        assert ts_in > ts_out
         assert ts_in % ts_out == 0
-        augs.append(TiltSeries(ts_in))
+        if ts_n == 1: 
+            augs.append(TiltSeries(ts_in, projections=[NormalView(ts_in)]))
+        else:
+            assert ts_n == 5
+            augs.append(TiltSeries(ts_in))
         augs.append(SubsampleLabels(factor=(ts_out,1,1)))
         if tilt_series_crop is not None:
             augs.append(CropLabels(tilt_series_crop))
