2a466da1bfce278e8d95db9345af4790cad0c1bd
NT <nturner@zetta.ai>
2022-08-10 15:46:16 -0400

    feat: combined dataset file

---
+++ deepem/data/dataset/pg_fly_larva_001/hemibrain+larva_lot.py
@@ -0,0 +1,71 @@
+import os
+
+import numpy as np
+import cloudvolume as cv
+from cloudfiles import CloudFiles
+
+import dataprovider3.emio as emio
+
+
+hemibrain_dir = 'gs://zetta-prieto-godino-fly-larva-001-seg-temp/seg-dataset/hemibrain'
+larva_dir = 'gs://zetta-prieto-godino-fly-larva-001-seg-temp/seg-dataset/larva'
+data_keys = [f"h{i:03d}" for i in range(8)] + [f"l{i:03d}" for i in range(2)]
+
+
+def load_data(base_dir, data_ids=None, **kwargs):
+    if data_ids is None:
+        return {}
+
+    data = {}
+    for data_id in data_ids:
+        if data_id in data_keys:
+
+            if data_id.startswith("h"):  # hemibrain
+                samplepath = os.path.join(hemibrain_dir, data_id)
+            elif data_id.startswith("l"):  # larva
+                samplepath = os.path.join(larva_dir, data_id)
+
+            cf = CloudFiles(samplepath)
+            sampleinfo = cf.get_json("info")
+
+            data[data_id] = load_dataset(samplepath, sampleinfo, **kwargs)
+
+        else:
+            raise KeyError(f"invalid data id: {data_id}")
+
+    return data
+
+
+def load_dataset(dpath, info, **kwargs):
+    dset = {}
+
+    # Image
+    vers = "000"
+    fpath = os.path.join(dpath, "image", vers)
+    print(fpath)
+    dset['img'] = cv.CloudVolume(fpath)[:].transpose(3, 2, 1, 0)[0, ...]
+    dset['img'] = (dset['img'] / 255.).astype(np.float32)
+
+    # Segmentation
+    vers = sorted(info["annotations"]["seg"]["versions"].keys())[-1]
+    fpath = os.path.join(dpath, "seg", vers)
+    print(fpath)
+    dset['seg'] = cv.CloudVolume(fpath)[:].transpose(3, 2, 1, 0)[0, ...]
+
+    # Additoinal info
+    dset['loc'] = True
+
+    # Mask
+    seg = dset['seg']
+    dset['msk'] = np.zeros(seg.shape, dtype=np.uint8)
+    dset['msk'][128:-128, 128:-128, 128:-128] = 1
+
+    return dset
+
+
+if __name__ == "__main__":
+    data = load_data(None, data_ids=["000"])
+
+    print(data["000"]["img"].shape)
+    print(data["000"]["seg"].shape)
+    print(data["000"]["msk"].shape)
