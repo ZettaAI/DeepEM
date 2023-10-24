738dbfe6cba5aea9a988bb93e510e828426963d9
Kisuk Lee <kisuk@zetta.ai>
2023-10-24 19:02:00 +0900

    fix(options): enable cuDNN auto-tuner by default

--- deepem/train/option.py
+++ deepem/train/option.py
@@ -34,7 +34,7 @@ class Options(object):
         self.parser.add_argument('--samwise_period', type=int, default=600)        
 
         # cuDNN auto-tuning
-        self.parser.add_argument('--no_autotune', action='store_false')
+        self.parser.add_argument('--no_autotune', action='store_true')
 
         # Training/validation sets
         self.parser.add_argument('--train_ids', type=str, default=[], nargs='+')
