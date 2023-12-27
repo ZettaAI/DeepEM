9e6648c5594d2dd75efa033c8c936c68dd1fb952
Kisuk Lee <kisuk@zetta.ai>
2023-12-27 23:36:07 +0900

    feat(sampler): introduce `DataSuperset` support and enhance code quality

--- deepem/train/data.py
+++ deepem/train/data.py
@@ -53,7 +53,8 @@ class Data(object):
         # Data sampler
         mod = imp.load_source('sampler', opt.sampler)
         spec = mod.get_spec(opt.in_spec, opt.out_spec)
-        sampler = mod.Sampler(data, spec, is_train, aug, prob=prob)
+        zspecs = opt.zettaset_specs
+        sampler = mod.Sampler(data, spec, is_train, aug, prob, zspecs)
 
         # Sample modifier
         if opt.modifier:
