ba495e31f509b396c088a5bda3ac8ee33da58a1c
Kisuk Lee <kisuk@zetta.ai>
2024-03-01 15:47:24 +0900

    feat(augment): add 16nm data augmentation for fly

---
+++ deepem/data/augment/flyem/aug_mip2.py
@@ -0,0 +1,102 @@
+from augmentor import *
+
+
+def get_augmentation(
+    is_train,
+    box=None,
+    missing=7,
+    blur=7,
+    lost=True,
+    random=False,
+    recompute=False,
+    border=False,
+    **kwargs
+):
+    augs = list()
+
+    # Brightness & contrast purterbation
+    augs.append(
+        MixedGrayscale2D(
+            contrast_factor=0.5,
+            brightness_factor=0.5,
+            prob=1, skip=0.3))
+
+    # Mutually exclusive augmentations
+    mutex = list()
+
+    # (1) Misalingment
+    trans = Compose([Misalign((0, 3), margin=1),
+                     Misalign((0, 8), margin=1),
+                     Misalign((0, 13), margin=1)])
+    slip = Compose([SlipMisalign((0, 3), interp=True, margin=1),
+                    SlipMisalign((0, 8), interp=True, margin=1),
+                    SlipMisalign((0, 13), interp=True, margin=1)])
+    mutex.append(Blend([trans, slip], props=[0.7, 0.3]))
+
+    # (2) Misalignment + missing section
+    if is_train:
+        mutex.append(Blend([
+            MisalignPlusMissing((2, 8), value=0, random=random),
+            MisalignPlusMissing((2, 8), value=0, random=False)
+        ]))
+    else:
+        mutex.append(MisalignPlusMissing((2, 8), value=0, random=False))
+
+    # (3) Missing section
+    if missing > 0:
+        if is_train:
+            mutex.append(Blend([
+                MixedMissingSection(maxsec=missing, individual=True, value=0, random=False),
+                MixedMissingSection(maxsec=missing, individual=True, value=0, random=random),
+                MissingSection(maxsec=missing, individual=False, value=0, random=random),
+            ]))
+        else:
+            mutex.append(
+                MixedMissingSection(maxsec=missing, individual=True, value=0, random=False)
+            )
+
+    # (4) Lost section
+    if lost:
+        if is_train:
+            mutex.append(Blend([
+                LostSection(1),
+                LostPlusMissing(value=0, random=random),
+                LostPlusMissing(value=0, random=False)
+            ]))
+
+    # Mutually exclusive augmentations
+    augs.append(Blend(mutex))
+
+    # Box
+    if is_train:
+        if box == 'noise':
+            augs.append(
+                NoiseBox(sigma=(1,3), dims=(3, 13), margin=(1, 3, 3),
+                         density=0.3, skip=0.1)
+            )
+        elif box == 'fill':
+            augs.append(
+                FillBox(dims=(3, 13), margin=(1, 3, 3),
+                        density=0.3, skip=0.1)
+            )
+
+    # Out-of-focus section
+    if blur > 0:
+        augs.append(MixedBlurrySection(maxsec=blur))
+
+    # Warping
+    if is_train:
+        augs.append(Warp(skip=0.3, do_twist=False, rot_max=45.0, scale_max=1.1))
+
+    # Recompute connected components
+    if recompute:
+        augs.append(Label())
+
+    # Flip & rotate
+    augs.append(FlipRotate())
+
+    # Create border
+    if border:
+        augs.append(Border())
+
+    return Compose(augs)
