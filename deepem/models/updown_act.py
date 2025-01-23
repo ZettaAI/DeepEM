4a47015fa350a4c9329a63d7c20b1832c86494e7
Kisuk Lee <kisuk@zetta.ai>
2025-01-23 17:19:24 +0900

    fix: update UpBlock of up-down net to support ONNX export

--- deepem/models/updown_act.py
+++ deepem/models/updown_act.py
@@ -20,7 +20,7 @@ def create_model(opt):
     else:
         # Batch normalization
         core = rsunet_act(width=width[:depth], act=opt.act)
-    return Model(core, opt.in_spec, opt.out_spec, width[0], crop=opt.crop)
+    return Model(core, opt.in_spec, opt.out_spec, width[0], crop=opt.crop, onnx=opt.onnx)
 
 
 class InputBlock(nn.Sequential):
@@ -30,15 +30,19 @@ class InputBlock(nn.Sequential):
 
 
 class OutputBlock(nn.Module):
-    def __init__(self, in_channels, out_spec, kernel_size):
+    def __init__(self, in_channels, out_spec, kernel_size, onnx=False):
         super(OutputBlock, self).__init__()
+        self.onnx = onnx
         for k, v in out_spec.items():
             out_channels = v[-4]
             self.add_module(k,
                     Conv(in_channels, out_channels, kernel_size, bias=True))
 
     def forward(self, x):
-        return {k: m(x) for k, m in self.named_children()}
+        if self.onnx:
+            return tuple(m(x) for k, m in self.named_children())
+        else:
+            return {k: m(x) for k, m in self.named_children()}
 
 
 class DownBlock(nn.Sequential):
@@ -48,14 +52,18 @@ class DownBlock(nn.Sequential):
 
 
 class UpBlock(nn.Module):
-    def __init__(self, out_spec, scale_factor=(1,2,2)):
+    def __init__(self, out_spec, scale_factor=(1,2,2), onnx=False):
         super(UpBlock, self).__init__()
+        self.onnx = onnx
         for k, v in out_spec.items():
             self.add_module(k,
                     nn.Upsample(scale_factor=scale_factor, mode='trilinear'))
 
     def forward(self, x):
-        return {k: m(x[k]) for k, m in self.named_children()}
+        if self.onnx:
+            return tuple(m(x[i]) for i, (_, m) in enumerate(self.named_children()))
+        else:
+            return {k: m(x[k]) for k, m in self.named_children()}
 
 
 class Model(nn.Sequential):
@@ -63,7 +71,7 @@ class Model(nn.Sequential):
     Residual Symmetric U-Net with down/upsampling in/output.
     """
     def __init__(self, core, in_spec, out_spec, out_channels, io_kernel=(1,5,5),
-                 scale_factor=(1,2,2), crop=None):
+                 scale_factor=(1,2,2), crop=None, onnx=False):
         super(Model, self).__init__()
 
         assert len(in_spec)==1, "model takes a single input"
@@ -72,7 +80,7 @@ class Model(nn.Sequential):
         self.add_module('down', DownBlock(scale_factor=scale_factor))
         self.add_module('in', InputBlock(in_channels, out_channels, io_kernel))
         self.add_module('core', core)
-        self.add_module('out', OutputBlock(out_channels, out_spec, io_kernel))
-        self.add_module('up', UpBlock(out_spec, scale_factor=scale_factor))
+        self.add_module('out', OutputBlock(out_channels, out_spec, io_kernel, onnx=onnx))
+        self.add_module('up', UpBlock(out_spec, scale_factor=scale_factor, onnx=onnx))
         if crop is not None:
             self.add_module('crop', Crop(crop))
